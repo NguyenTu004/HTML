27449aff6dc6b0b38c5149b9c4edef11098c7c67
tunguyen <nngoctu004@gmail.com>
2022-09-08 10:02:22 +0700

    08/09

---
+++ 0809js1/0809js1.js
@@ -0,0 +1,16 @@
+let i = 10
+let f = 20.5
+let b = true
+let c = 'Hà Nội'
+document.write('i= '+ i)
+document.write('<br>')
+document.write('f= '+ f)
+document.write('<br>')
+document.write('b= '+ b)
+document.write('<br>')
+document.write('c= '+ c)
+document.write('<br>')
+let width = 40
+let height = 25
+let s = width*height
+document.write("s= "+ s)
