7c224f36a6ddc16e4c50a70f50a953d33b74a13d
tunguyen <nngoctu004@gmail.com>
2022-09-08 13:42:39 +0700

    08/09

--- 0809js1/0809js1.js
+++ 0809js1/0809js1.js
@@ -14,3 +14,11 @@ let width = 40
 let height = 25
 let s = width*height
 document.write("s= "+ s)
+
+let a= +prompt("nhập a")
+let b= +prompt("nhập b")
+if (a%b==0){
+    alert("a chia hết cho b")
+}else {
+    alert("a không chia hết cho b")
+}
